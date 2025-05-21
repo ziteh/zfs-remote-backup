ba22757449e208b788b84eac943d07afbd830503
ziteh <honmonoh@gmail.com>
2025-05-21 15:21:33 +0000

    test: integrate mockall for unit testing in status manager

--- zrb/src/status/manager.rs
+++ zrb/src/status/manager.rs
@@ -1,3 +1,6 @@
+use mockall::{automock, predicate::*};
+
+#[automock]
 trait FileIo {
     fn load_target_queue(&self) -> Result<BackupTargetQueue, Error>;
     fn load_active_tasks(&self) -> Result<ActiveBackupTask, Error>;
@@ -45,5 +48,57 @@ impl StatusManager {
         } else {
             Err(Error)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::Utc;
+
+    fn sample_target(name: &str) -> BackupTarget {
+        BackupTarget {
+            date: Utc::now(),
+            backup_type: BackupType::Full,
+            dataset: name.to_string(),
+        }
+    }
+
+    #[test]
+    fn test_enqueue_and_dequeue() {
+        let mut mock_io = MockFileIo::new();
+
+        mock_io
+            .expect_load_target_queue()
+            .returning(|| Ok(VecDeque::new()));
+
+        mock_io
+            .expect_load_active_tasks()
+            .returning(|| Ok(ActiveBackupTask::default()));
+
+        mock_io
+            .expect_load_latest_snapshot_map()
+            .returning(|| Ok(LatestSnapshotMap::default()));
+
+        mock_io
+            .expect_save_target_queue()
+            .times(2)
+            .returning(|_| Ok(()));
+
+        let mut manager = StatusManager::new(Box::new(mock_io));
+
+        let target1 = sample_target("pool1/data1");
+        manager.enqueue_target(target1.clone()).unwrap();
+
+        let target2 = sample_target("pool1/data2");
+        manager.enqueue_target(target2.clone()).unwrap();
+
+        let out1 = manager.dequeue_target().unwrap();
+        assert_eq!(out1.dataset, target1.dataset);
+
+        let out2 = manager.dequeue_target().unwrap();
+        assert_eq!(out2.dataset, target2.dataset);
+
+        assert!(manager.dequeue_target().is_err());
     }
 }
