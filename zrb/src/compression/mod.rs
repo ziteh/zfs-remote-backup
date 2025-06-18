c59273d8bf20409c62620531d607595d641c818d
ziteh <honmonoh@gmail.com>
2025-06-18 23:50:04 +0000

    feat: add Age encryptor

--- zrb/src/compression/mod.rs
+++ zrb/src/compression/mod.rs
@@ -1,6 +1,5 @@
 pub mod manager;
 pub mod zstd_compressor;
-pub mod example;
 
 pub use manager::Compressor;
 pub use zstd_compressor::ZstdCompressor;
--- zrb/src/encryption/mod.rs
+++ zrb/src/encryption/mod.rs
@@ -1,1 +1,5 @@
+pub mod age_encryptor;
 pub mod manager;
+
+pub use age_encryptor::AgeEncryptor;
+pub use manager::Encryptor;
