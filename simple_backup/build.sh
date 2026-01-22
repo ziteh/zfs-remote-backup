2c7e008f0c8169be8742956caa57903985921c8a
ZiTe <honmonoh@gmail.com>
2026-01-22 22:29:15 +0800

    chore: add multipass commands to transfer

--- simple_backup/build.sh
+++ simple_backup/build.sh
@@ -4,3 +4,8 @@
 GOOS=linux GOARCH=amd64 go build -o build/zrb_simple_linux_amd64
 
 GOOS=linux GOARCH=arm64 go build -o build/zrb_simple_linux_arm64
+
+# VM Setup
+multipass transfer build/zrb_simple_linux_arm64 zfs-minio:/tmp/
+multipass exec zfs-minio -- sudo chmod +x /tmp/zrb_simple_linux_arm64
+multipass exec zfs-minio -- sudo /tmp/zrb_simple_linux_arm64 -h
