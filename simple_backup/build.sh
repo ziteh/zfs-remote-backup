ba9887c69dea77f2cb7e80aea13fde791cded66b
ZiTe <honmonoh@gmail.com>
2026-02-08 09:26:20 +0800

    feat: add build flags for optimized Go binaries

--- simple_backup/build.sh
+++ simple_backup/build.sh
@@ -2,12 +2,15 @@
 
 VM="zrb-vm"
 
-go build -o build/zrb_simple_native
+# Build flags: -s removes symbol table, -w removes DWARF debugging info
+LDFLAGS="-s -w"
+
+go build -ldflags="$LDFLAGS" -o build/zrb_simple_native
 
 # For TrueNAS CE/Scale (Debian based)
-GOOS=linux GOARCH=amd64 go build -o build/zrb_simple
+GOOS=linux GOARCH=amd64 go build -ldflags="$LDFLAGS" -o build/zrb_simple
 
-GOOS=linux GOARCH=arm64 go build -o build/zrb_simple_linux_arm64
+GOOS=linux GOARCH=arm64 go build -ldflags="$LDFLAGS" -o build/zrb_simple_linux_arm64
 
 # VM Setup
 multipass transfer build/zrb_simple_linux_arm64 "$VM:/tmp/zrb_simple"
